3ee7a6452f061bb262068c3febc6807c66a1f526
wildmouse <wildmouse0813@gmail.com>
2022-08-03 14:30:03 +0900

    Add console

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import { setupCounter } from './counter.js'
 
+console.log(import.meta.env)
+
 document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
